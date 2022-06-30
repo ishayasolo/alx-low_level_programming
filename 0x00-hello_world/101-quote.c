ef1b069fdf294b5301e02f73018cd3155d9ed53a
ishayasolo <ishaya808@gmail.com>
2022-06-30 16:54:17 -0700

    add 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	fprintf("%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (0);
+}
