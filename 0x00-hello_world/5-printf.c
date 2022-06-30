f45b688d1a0bfd9bc2f0d594df04b9109b8a91c1
ishayasolo <ishaya808@gmail.com>
2022-06-30 13:36:49 -0700

    modify 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
