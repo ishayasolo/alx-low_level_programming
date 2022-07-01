04d6dca1c52801fa0e41f34b4e982a23c44d99f8
ishayasolo <ishaya808@gmail.com>
2022-07-01 10:27:38 -0700

    add 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char lowercase;
+	char uppercase;
+
+	for (lowercase='a'; lowercase <= 'z'; lowercase++)
+		putchar(lowercase);
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+		putchar(uppercase);
+
+	putchar('\n');
+
+	return (0);
+}
