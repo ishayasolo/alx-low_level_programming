db0707648451e979082b57061311379534d69e95
ishayasolo <ishaya808@gmail.com>
2022-07-01 11:59:43 -0700

    modify 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,18 @@ int main(void)
 {
 	int x;
 
-	putchar('0');
-
 	for (x = 1; x < 10; x++)
 	{
-		putchar(',');
-		putchar(' ');
 		putchar(x + '0');
+		if (x == 9)
+			break;
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 
 	return (0);
 }
