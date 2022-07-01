47b622e366ee4f4d142ba1f498b8d478472ba511
ishayasolo <ishaya808@gmail.com>
2022-07-01 10:31:18 -0700

    modify 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ int main(void)
 	char lowercase;
 	char uppercase;
 
-	for (lowercase='a'; lowercase <= 'z'; lowercase++)
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 		putchar(lowercase);
 	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 		putchar(uppercase);
