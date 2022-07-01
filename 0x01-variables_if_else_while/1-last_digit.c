4f403e2659ca6a5e534a91ace80d18efb2bba036
ishayasolo <ishaya808@gmail.com>
2022-07-01 10:21:05 -0700

    modify 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ int main(void)
 	printf("Last digit of %d is %d and is ", n, n % 10);
 
 	if (n % 10 > 5)
-		puts("greater than 5\n");
+		puts("greater than 5");
 	else if (n % 10 == 0)
-		puts("0\n");
+		puts("0");
 	else if (n % 10 < 6 && n % 10 != 0)
-		puts("less than 6 and not 0\n");
+		puts("less than 6 and not 0");
 
 	return (0);
 }
