288626710b45188f6585e50cbe060e22f514c2fd
ishayasolo <ishaya808@gmail.com>
2022-07-05 17:08:37 -0700

    modify print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,10 +13,9 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		{
+		for (alphabet = 'a'; alphabet < '{'; alphabet++)
 			_putchar(alphabet);
-		}
+
 		_putchar('\n');
 	}
 }
