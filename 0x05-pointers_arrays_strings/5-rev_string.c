817b3ec84cec68f75aa1159b5a18187dfff2649e
ishayasolo <ishaya808@gmail.com>
2022-07-12 15:06:28 -0700

    modify 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,13 +14,13 @@ void rev_string(char *s)
 {
 	int length = strlen(s);
 	int last_index = length - 1;
-	char new_string[];
+	char *new_string;
 	int i;
-	int j = 0;
+	int j = 1;
 
 	for (i = last_index; i >= 0; i--)
 	{
-		new_string[j] = *(s + i);
+		*(new_string + j) = *(s + i);
 		j++;
 	}
 
