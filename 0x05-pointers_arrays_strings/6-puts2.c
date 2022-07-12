6fd6718f3886954e251e2ed39f6da917eaace977
ishayasolo <ishaya808@gmail.com>
2022-07-12 16:21:00 -0700

    add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - prints characters in the odd index of a string
+ *
+ * @str: string to be evaluated
+ *
+ * Return: void (nothing)
+ */
+
+void puts2(char *str)
+{
+	int length = strlen(str);
+	int i;
+
+	for (i = 0; i < length - 1; i += 2)
+		putchar(str[i]
+}
