638519863dea73c30c2a0ebdf6f3fac8aa200eaf
ishayasolo <ishaya808@gmail.com>
2022-07-12 19:16:14 -0700

    modify 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 #include <stdlib.h>
 
 /**
@@ -11,5 +12,21 @@
 
 int _atoi(char *s)
 {
-	return (atol(s));
+	int num, digit, i;
+	
+	num = atoi(s);
+
+	if (num == 0)
+	{
+		for (i = 0; i < strlen(s); i++)
+		{
+			if (s[i] >= '0' && s[i] <= '9')
+			{
+				digit = s[i] - '0';
+				num  = (num * 10) + digit;
+			}
+		}
+	}
+	
+	return (num);
 }
