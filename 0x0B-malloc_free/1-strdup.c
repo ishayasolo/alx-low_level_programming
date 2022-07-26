b527b01d5638458877c5f013205bf36b5438bd60
ishayasolo <ishaya808@gmail.com>
2022-07-26 15:46:54 -0700

    add 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a new string which is a duplicate of the string str
+ * @str: string to be copied
+ *
+ * Return: pointer to a newly allocated space
+ */
+
+char *_strdup(char *str)
+{
+	str = maloc(sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+
+	return (*str);
+}
